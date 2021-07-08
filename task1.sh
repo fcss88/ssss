1ce5c6bb9f32cdd4e450793768ce77012790e4aa
root <root@MoodleNew.chost.com.ua>
2021-07-08 21:46:35 +0300

    :)

--- task1.sh
+++ task1.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
-# my first bashscript 
+# script task1
 clear
 echo "Task1 Script"
 
+# Am I root?
+if [ "x$(id -u)" != 'x0' ]; then
+    echo 'Error: this script can only be executed by root'
+    exit 1
+fi
+# I show that i can write if-fi combinatioan as you see
+# but in this script i wish use standart console answers
 echo user $1
 echo dir $2
 sudo chown -R $1 $2 
